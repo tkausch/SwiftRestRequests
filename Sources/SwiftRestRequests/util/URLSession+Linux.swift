d9cc0ab6f4b9276616a9cf766adc0df1b0f166d5
Thomas Kausch <thomas.kausch@swisscom.com>
2024-03-08 23:14:40 +0100

    Added implementation for async data task for linux - currently missing in FoundationNetworking as Apple is not supporting it :-(

---
+++ Sources/SwiftRestRequests/util/URLSession+Linux.swift
@@ -0,0 +1,41 @@
+//
+// Deserializer.swift
+//
+// This File belongs to SwiftRestRequests
+// Copyright © 2024 Thomas Kausch.
+// All Rights Reserved.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+
+extension URLSession {
+  func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+    return try await withCheckedThrowingContinuation { continuation in
+      let task = self.dataTask(with: request) { (data, response, error) in
+        guard let data = data, let response = response else {
+          let error = error ?? URLError(.badServerResponse)
+          return continuation.resume(throwing: error)
+        }
+        continuation.resume(returning: (data, response))
+      }
+      task.resume()
+    }
+  }
+}
+#endif
